80c2c1ae40b01983e9067d63cd1551f95603bc1e
Camden Rudisill <camden@planet.com>
2017-10-15 20:35:23 -0700

    point moving through trail based on speed

--- src/index.js
+++ src/index.js
@@ -30,22 +30,46 @@ first.forEach(d => {
   sortedByCheckpoint[checkpoints[d.Checkpoint]] = d; 
 });
 
+console.log('sortedByCheckpoint', sortedByCheckpoint); // (camden)
+
 // get coordinates of each checkpoint for geoJSON line layer of Iditarod trail 
 const checkpointCoords = sortedByCheckpoint.map(d => {
     return [+d.Longitude, +d.Latitude];
 });
 
-console.log('checkpointCoords', checkpointCoords); // (camden)
+const checkpointSpeeds = sortedByCheckpoint.map((d, index) => {
+    return +d.Time;
+});
 
-let route = {
-    "type": "FeatureCollection",
-    "features": [{
+checkpointSpeeds.shift();
+
+function getCheckpointPairs (coords) {
+    let pairs = [];
+    for (var i=0 ; i<coords.length ; i++) {
+        if (coords[i+1] !== undefined) {
+            pairs.push ([coords[i], coords[i+1]]);
+        } 
+    }
+
+    return pairs;
+} 
+
+function createFeature (long, lat) {
+    return {
         "type": "Feature",
         "geometry": {
             "type": "LineString",
-            "coordinates": checkpointCoords
+            "coordinates": [long, lat]
         }
-    }]
+    } 
+}
+
+let checkpointPairs = getCheckpointPairs(checkpointCoords);
+let cpFeatures = checkpointPairs.map(cp => createFeature(cp[0], cp[1]));
+
+let route = {
+    "type": "FeatureCollection",
+    "features": cpFeatures 
     
 };
 
@@ -60,19 +84,43 @@ let point = {
     }]
 };
 
-// // Calculate the distance in kilometers between route start/end point.
-let lineDistance = turf.lineDistance(route.features[0], 'kilometers');
+// distance between checkpoints in miles
+const distanceBetweenCheckpoints = cpFeatures.map(ft => turf.lineDistance(ft, 'miles'));
+// let lineDistance = turf.lineDistance(route.featuresk0], 'kilometers');
 
 let trail = [];
 
-//TODO: (camden) normalize speeds of mushers 
-// create list of segments along the trail 
-for (let i = 0; i < lineDistance * 2; i++) {
-    const segment = turf.along(route.features[0], (i / 2000) * lineDistance, 'kilometers');
-    trail.push(segment.geometry.coordinates);
+
+console.log('route.features', route.features); // (camden)
+console.log('distanceBetweenCheckpoints', distanceBetweenCheckpoints); // (camden)
+// route.features.forEach((feature, index) => {
+//     console.log('index', index); // (camden)
+//     const segment = turf.along(feature, (index / 1000) * distanceBetweenCheckpoints[index], 'miles');
+//     trail.push(segment.geometry.coordinates); 
+// });
+
+let last;
+for (let i = 0; i < route.features.length; i++) {
+    for (let j = 0; j < (distanceBetweenCheckpoints[i] / Math.ceil(checkpointSpeeds[i])) * 100; j++) {
+        const segment = turf.along(route.features[i], (j/100) * distanceBetweenCheckpoints[i], 'miles');
+        if (trail.length && segment.geometry.coordinates[0] === trail[trail.length-1][0]) {
+            console.log('here'); // (camden) 
+        } else {
+            trail.push(segment.geometry.coordinates);  
+        }
+    }
 }
 
-// Update the route with calculated arc coordinates
+console.log('trail', trail); // (camden)
+
+// // // create list of segments along the trail 
+// for (let i = 0; i < lineDistance * 2; i++) {
+//     const segment = turf.along(route.features[0], (i / 2000) * lineDistance, 'kilometers');
+//     trail.push(segment.geometry.coordinates);
+// }
+
+
+// // Update the route with calculated arc coordinates
 route.features[0].geometry.coordinates = trail;
 
 mapboxgl.accessToken = Config.API_KEY;
@@ -100,8 +148,6 @@ let counter = 0;
 
 map.on('load', function () {
 
-    console.log('trail.length', trail.length); // (camden)
-   
     map.addSource('route', {
         "type": "geojson",
         "data": route
@@ -117,22 +163,6 @@ map.on('load', function () {
         }
     });
 
-    // map.addLayer({
-    //     "id": "route",
-    //     "type": "line",
-    //     "source": {
-    //         "type": "geojson",
-    //         "data": route 
-    //     },
-    //     "layout": {
-    //         "line-join": "round",
-    //         "line-cap": "round"
-    //     },
-    //     "paint": {
-    //         "line-color": "#fd00ff",
-    //         "line-width": 2
-    //     }
-    // });
     map.addSource('point', {
         "type": "geojson",
         "data": point 
@@ -149,9 +179,14 @@ map.on('load', function () {
     });
 
     
+    let routeInd = 0;
     function play() {
         // Update point geometry to a new position based on counter denoting
         // the index to access the arc.
+        if (counter >= route.features[0].geometry.coordinates.length) {
+            counter = null;
+        }
+        if (counter === null) return;
         point.features[0].geometry.coordinates = route.features[0].geometry.coordinates[counter];
 
         // Update the source with this new data.
